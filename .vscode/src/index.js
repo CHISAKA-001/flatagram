100b61e9f3edc68e25ee8d3bfa2ac58c56e09a63
CHISAKA-001 <eddyc00de@gmail.com>
2022-09-07 13:16:10 +0300

    Complete

--- .vscode/src/index.js
+++ .vscode/src/index.js
@@ -2,35 +2,36 @@
 document.addEventListener ("DOMContentLoaded", onLoad);
 let log = console.log();
 
-const image  = document.getERlementById(".image");
-const title = document.getElementById(".title");
-const like = documen.getElementById(".like-count");
-const likeBtn = document.getElementById(".like-button");
-const comments = doucument.getERlementById(".comments");
+const image  = document.querySelector(".image");
+const title = document.querySelector(".title");
+const likes = documen.querySelector(".likes");
+const likeBtn = document.querySelector(".like-button");
+const comments = doucument.querySelector(".comments");
 
 function onLoad() {
     return fetch ("http://localhost:3000/images/1")
         .then ((resp) =>  resp.json())
         .then ((data) => {
-            updateImage(data);
+            loadImage(data);
         });
 }
 
-function updateImage(data) {
+function loadImage(data) {
     image.scr = data.image 
     title.textContent = data.title;
 
-}
-
-likeBtn.addEventListener("click" , () => {
-    let updateLikes = data.likes +1
-    likes.textContent = updateLikes + "likes";
-    log(updateLikes)
+likeBtn.addEventListener("click", () => {
+    let updateLikes = data.likes ++;
+    likes.textContent = updateLikes + " likes";
+    log(updateLikes);
 })
 
 comments.innerText = " "
 data.comments.forEach(element => {
     let li = document.createElement("li")
     li.appeand(element.content)
-    comments.append(li)
+    comments.append(li);
 });
+
+}
+
